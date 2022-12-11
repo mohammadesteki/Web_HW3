25b02c942ce1f63c612907487b980e3eb22e0fb9
kamyardl <kamdl1379@yahoo.com>
2022-12-10 22:19:40 -0800

    FEATURE(): complete front phase

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-// Get the .click-me element
-let clickMe = document.querySelector('.clickkk');
-// This will run when the .click-me element is clicked
-// console.log("clickme", clickMe)
-clickMe.addEventListener('click', function (event) {
-    alert("fuck")
-    redirect("login")
+// import Datepicker from './node_modules/vanillajs-datepicker/js/Datepicker.js';
+
+// // Get the .click-me element
+// let clickMe = document.querySelector('.clickkk');
+// // This will run when the .click-me element is clicked
+// // console.log("clickme", clickMe)
+// clickMe.addEventListener('click', function (event) {
+//     alert("fuck")
+//     redirect("login")
+// });
+
+
+// const elem = document.querySelector('#fuckme');
+// console.log(elem);
+// const datepicker = new Datepicker(elem, {
+//   // ...options
+// //   alert("fuck")
+
+// //   ("fuck")
+// }); 
+
+
+
+$('[data-toggle=offcanvas]').click(function () {
+    $('.trial2').toggleClass('hide');
+});
+
+$('[data-toggle=offcanvas1]').click(function () {
+    $('.trial').toggleClass('hide');
 });
